f6a30c7b66bd9a9398fcd1208a99c455426f648a
Abdulla-Ababakre <abdullaszu@gmail.com>
2023-11-02 01:39:24 -0700

    feat: pdf upload

--- pages/api/pdf-upload.js
+++ pages/api/pdf-upload.js
@@ -13,7 +13,6 @@ export default async function handler(req, res) {
       "/Users/abdullaababakre/Desktop/text-chatbot/data/document_loaders/AbdullaData.pdf";
     const loader = new PDFLoader(bookPath);
 
-    
 
     const docs = await loader.load();
     console.log("docs11", docs);
