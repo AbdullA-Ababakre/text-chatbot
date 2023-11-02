d81242ca3221f81e3f2b23db70e81b33ad5f7394
Abdulla-Ababakre <abdullaszu@gmail.com>
2023-11-02 00:56:33 -0700

    feat

--- pages/api/pdf-upload.js
+++ pages/api/pdf-upload.js
@@ -10,8 +10,10 @@ export default async function handler(req, res) {
     // console.log("Uploading book");
     /** STEP ONE: LOAD DOCUMENT */
     const bookPath =
-      "https://ff23ef6721c027b7233dd9bb1903a69c6e784367ecd0211a64a05d7-apidata.googleusercontent.com/download/storage/v1/b/abdullapdfdata/o/AbdullaData.pdf?jk=AQHBpxy0H5-WqQauY5GWWEpygPcBqdw7X627d-vaLjI2legglWgqrYFCXBGwZA5E8dTu42SaFpssmXxEsMnHE-WmTAjlDId_ma8YjNVhbbyAsOhVJxNpwapLgYSF3ZVp0RPw6FHBh8HgnSX0XUsA5PuxL7xn4Z0xviOD0xkjLIBv2KAmURbJV_4rfsLoqnxcGkFrsbMS_LtXjUbOCNd2I_V0Brbd6RNZwaMpkb2OATgBs84NQB1AzAVm7Y4cZl5ybgmVK0g06SrsNpsToC-3xnTU3_lhZgBLkBI8CTOAcsVLdOydTQW1H1rcElRfvDnUfrhmx15wl3mqOpYe5vu5MIiTeFIglPVBVMVMkcGwhLb4vci4W9kxuZp3_2hSRcag3h4LEnU6_Z3IBL8i8L8tSHdrpcFUOeWMIwzn-C3-uPT3ZbJd2w4KhL3kpu9Rg7raoSYwUKZ-2RxlKSImEfolpZEddK-xZwBp_rvXcaCoVVrCG9OLoJkkEe5sdo5uPG5NiwQv9V9VgEVemf2iqDRYCwqgDeS2IIbLr7CLLNBdtD4Cm1kNV71chPaA9bZKsq5NA1aPYFE1_2Vy-7yST-IRGuYxENJVZMxyYmbYFKR_5xbMjX2rEsv7aJeW08CPANaFGz6e6E2suSQN2Q4MDRL5UHuE-7qeU1tAYjEN5RA_7nZSouy35nt-TXpMDn3XqA4hhH7QzGH8RTtlXj-f-uavoyfSk3wbwMNkAToaigw7UMlOAkmapFB9RaTOa5MeBC8HH-WXr3-qnQaMMEyWSMmiBHgT5ygYtNGLn1DtAe1rXldMtJGeIBOVhcqCeecLoMpWDwl3z95RzK0U8r6ho7I7CxIazEvOSraZYGI9RLf3oxMwgwaYRolXYfYvvEBm5t7hr7-hBESUNroKekHE6Ws7tpYxFKd7qz7D8kOgzTcr2WsmDjzQoZv8OrpaQ_tGkFOBdog778_dP9s9q8_cvSPCBtsdbfLYcegFRro6MO-WZ1-pxADVtyTXLCOt9uDpLhqTwEvIho1-0ofoVZf2HyI579Ft5jN0ZmZD3pTY27VAkwqkmxMMnYVD_kmzmDKsk3KTF2IteHIrmo-gs-ylk2xmoC4brHp7doHb9neszIe0DKPH7EZS-GTr05T55d4Moa5RqJSVxa2A4pswbHihYjJmtEr4et7W&isca=1";
+      "/Users/abdullaababakre/Desktop/text-chatbot/data/document_loaders/AbdullaData.pdf";
     const loader = new PDFLoader(bookPath);
+
+    
 
     const docs = await loader.load();
     console.log("docs11", docs);
