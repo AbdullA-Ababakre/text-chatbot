27d2b68f6d2f8a9f9f5538615db74a08471958fb
Abdulla-Ababakre <abdullaszu@gmail.com>
2023-11-16 22:40:28 -0800

    fix: remove left child type

--- src/app/components/TwoColumnLayout.jsx
+++ src/app/components/TwoColumnLayout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const TwoColumnLayout = ({ leftChildren, rightChildren }) => (
+const TwoColumnLayout = ({ rightChildren }) => (
   <div className="flex flex-col justify-between  md:flex-row md:justify-between">
     {/* Chat */}
     <div className="md:w-2/5 w-full min-h-screen">{rightChildren}</div>
